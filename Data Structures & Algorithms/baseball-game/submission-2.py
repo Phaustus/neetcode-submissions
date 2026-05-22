cbea509567607e9a3cc473ce5e439f8fbcac1283
0xC000 0000 <37073219+Phaustus@users.noreply.github.com>
2026-05-22 14:21:48 +0200

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        r = []
+        for i, val in enumerate(operations):
+            if val != "C" and val != "+" and val != "D":
+                r.append(int(val))
+            elif val == "+":
+                r.append(int(r[-1]) + int(r[-2]))
+            elif val == "C":
+                r.pop()
+            elif val == "D":
+                h = 2 * int(r[-1])
+                r.append(h)
+
+        return sum(r)
