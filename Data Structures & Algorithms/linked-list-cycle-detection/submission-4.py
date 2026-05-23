bb3ce08451c5bc03d9fc321ede1153625dead7dd
0xC000 0000 <37073219+Phaustus@users.noreply.github.com>
2026-05-23 20:53:44 +0200

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # lista koja sadrzi kopije svih node-ova pa ih uporedi svaki put kada naidje na novi
+        l = set()
+
+        while head != None:
+            if head in l:
+                return True
+            else:
+                l.add(head)
+            head = head.next
+        return False
