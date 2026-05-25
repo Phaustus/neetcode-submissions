2cbb1fb9a6e92efce0b5732c4fd1d15639e2a16c
0xC000 0000 <37073219+Phaustus@users.noreply.github.com>
2026-05-25 13:59:20 +0200

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    a = input()
+    b = a.split(',')
+    c = int(b[0]) + int(b[1])
+    return c
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
