29e2775c5926ab12fb4c1e5d6d5e99f967ad3e04
0xC000 0000 <37073219+Phaustus@users.noreply.github.com>
2026-05-25 08:24:08 +0200

    Add: python-multiple-assignment - submission-0

---
+++ Python For Beginners/python-multiple-assignment/submission-0.py
@@ -0,0 +1,13 @@
+msg2, msg1 = "World", "Hello"
+msg5, msg3, msg4 = "Name", "Is", "My"
+# Don't change the code above this line
+
+
+
+
+# Don't change the code below this line
+print(msg1)
+print(msg2)
+print(msg4)
+print(msg5)
+print(msg3)
