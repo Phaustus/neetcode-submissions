f2e1ed6d67dee11a5dd63115d90d7c61f5f36277
0xC000 0000 <37073219+Phaustus@users.noreply.github.com>
2026-05-25 11:49:00 +0200

    Add: python-reversing-a-string - submission-0

---
+++ Python For Beginners/python-reversing-a-string/submission-0.py
@@ -0,0 +1,12 @@
+def reverse_string(input_string: str) -> str:
+    start = 0
+    end = len(input_string)
+    return input_string[::-1]
+
+
+
+
+# do not modify below this line
+print(reverse_string("NeetCode"))
+print(reverse_string("Hello!"))
+print(reverse_string("Bye Bye"))
